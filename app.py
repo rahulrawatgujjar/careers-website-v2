61eb36824546bce8a65fbc18e69da49bd92431cd
Replit user <>
2024-01-29 13:26:50 +0000

    added home.html file and our websiste is now looking better

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "hello i am rahul"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
