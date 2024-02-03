987899545b30d59dbbe645fc11a24dd46dd67579
Replit user <>
2024-02-03 17:09:47 +0000

    added applications to database

--- database.py
+++ database.py
@@ -26,3 +26,18 @@ def load_job_from_db(id):
       return None
     else:
       return jobs[0]._asdict()
+
+def add_application_to_db(id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications( job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ( :job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    parameters = {
+        'job_id': id,
+        'full_name': data["full_name"],
+        'email': data["email"],
+        'linkedin_url': data["linkedin_url"],
+        'education': data["education"],
+        'work_experience': data["work_experience"],
+        'resume_url': data["resume_url"]
+    }
+    conn.execute(query, parameters)
+    conn.commit()
